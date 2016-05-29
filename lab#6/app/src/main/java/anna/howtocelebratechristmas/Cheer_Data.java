27b06d3a3a3a6d84035d4bd4970a143fc1940bc0
Artiom <artiombotoroga@gmail.com>
2016-05-29 19:02:43 +0400

    Add a part of LAb6

---
+++ lab#6/app/src/main/java/anna/howtocelebratechristmas/Cheer_Data.java
@@ -0,0 +1,186 @@
+package anna.howtocelebratechristmas;
+
+import android.graphics.Color;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cheer_Data {
+
+    private Cheer_Data() {
+    }
+
+    public static List<Information> load_cheer_items() {
+        List<Information> items = new ArrayList<>();
+
+        Information cheer_information_1 = new Information();
+        cheer_information_1.name = "C++ ";
+        cheer_information_1.description = "C++ este un lmbaj de baza a tuturor software" +
+                "pe care le folosim zi de zi accesind o simpla pagina web.";
+        cheer_information_1.color = Color.rgb(240, 280, 255);
+        cheer_information_1.image = "";
+        cheer_information_1.URL = "https://ru.wikipedia.org/wiki/C%2B%2B";
+
+        items.add(cheer_information_1);
+
+        Information cheer_information_2 = new Information();
+        cheer_information_2.name = "Avantaje";
+        cheer_information_2.description = "Este un limbaj pe programarea orientata pe obiecte. " +
+                "Se poste creea si un intreg sistem de operare.";
+        cheer_information_2.color = Color.rgb(111, 11, 111);
+        cheer_information_2.image = "";
+        cheer_information_2.URL = "https://ru.wikipedia.org/wiki/C%2B%2B";
+
+        items.add(cheer_information_2);
+
+
+        Information cheer_information_3 = new Information();
+        cheer_information_3.name = "Istorie ";
+        cheer_information_3.description = "Acest soft C++ si-a luat nastere in anul 1980 de catre Бьёрн Страуструп de la compania Bell Labs,modificind insasi limbaju de baza C adaugind 80% de modificari .\n" +
+                "\n" +
+                "Бьёрн Страуструп in anul 1970 cin a inceput activitatea in Bell a spus ca e prea complicat de facut legatura cu limbajul C cu celelalte limbaje deja existente.\n" +
+                "\n" +
+                "Si din momentul cela sa pus labza crearea unui nou limbaj cu modificari radicale.  \n" +
+                "\n" +
+                "A fost un limbaj foarte rapid,de ori care nu ar fin incarcat el,de aceea si sa adaugat la denumire ++.\n" +
+                "\n" +
+                "De aici deja o aparut clase si structui in limbaj C++.\n" +
+                "\n" +
+                "In anul 1985 a iesit prima carte de programare in C++.\n" +
+                "\n" +
+                "paid for someones Red Box\n" +
+                "\n" +
+                "\n" +
+                "\n" +
+                "\n" +
+                "\n" +
+                "\n" +
+                "\n" +
+                "\n" +
+                "\n" +
+                "\n" +
+                "\n" +
+                "\n" +
+                "\n" +
+                "\n" +
+                "\n" +
+                "\n" +
+                "\n" +
+                "\n" +
+                "\n" +
+                "\n" +
+                "\n" +
+                "Paid for the person behind us at Jack in the Box";
+        cheer_information_3.color = Color.rgb(13, 55, 125);
+        cheer_information_3.image = "";
+        cheer_information_3.URL = "http://erikandkellyahlander.blogspot.com/2011/12/24-acts-of-service.html";
+
+        items.add(cheer_information_3);
+
+
+        Information cheer_information_4 = new Information();
+        cheer_information_4.name = "Call up and check up on a friend ";
+        cheer_information_4.description = "Not everyone is so lucky to have" +
+                " family and friends. \n\n" +
+                "Check up on your friends who may be spending their holidays alone," +
+                " and invite them to your Christmas celebrations.\n\n";
+        cheer_information_4.color = Color.rgb(222, 22, 222);
+        cheer_information_4.image = "";
+        cheer_information_4.URL = "";
+        items.add(cheer_information_4);
+
+
+        Information cheer_information_5 = new Information();
+        cheer_information_5.name = "Read a classic Christmas story ";
+        cheer_information_5.description = "Read a novel with a Christmas theme, " +
+                "such as A Christmas Carol by Charles Dickens or " +
+                "Little Women by Lousia May Alcott.";
+        cheer_information_5.color = Color.rgb(122, 22, 222);
+        cheer_information_5.image = "";
+        cheer_information_5.URL = "https://en.wikipedia.org/wiki/Little_Women";
+        items.add(cheer_information_5);
+
+
+
+        Information cheer_information_6 = new Information();
+        cheer_information_6.name = "Take family photos and videos ";
+        cheer_information_6.description = "Taking home photos and videos area great way to preserve " +
+                "memories!";
+        cheer_information_6.color = Color.rgb(112, 22, 222);
+        cheer_information_6.image = "";
+        cheer_information_6.URL = "";
+        items.add(cheer_information_6);
+
+
+
+        Information cheer_information_7 = new Information();
+        cheer_information_7.name = "Call up and check up on an elderly neighbor ";
+        cheer_information_7.description = "A lot of older adults are alone at this time of year. " +
+                "Holidays are hard enough! Take care of your neighbors and " +
+                "help them out.";
+        cheer_information_7.color = Color.rgb(122, 122, 222);
+        cheer_information_7.image = "";
+        cheer_information_7.URL = "";
+        items.add(cheer_information_7);
+
+
+
+        Information cheer_information_8 = new Information();
+        cheer_information_8.name = "Spend time with your significant other ";
+        cheer_information_8.description = "Christmas Eve Traditions for Couples\n" +
+                "\n" +
+                "    Ornaments: I know a family that does this and I LOVE it. " +
+                "This can be a tradition just for the two of you, or you can make it" +
+                " a family tradition. Each year, before you go to bed on on Christmas" +
+                " Eve, you give each other an ornament that represents a favorite memory" +
+                " together that year. It could represent a vacation, something funny, " +
+                "or something meaningful. Another way to do this is to take turns buying" +
+                " the ornament each year. After several years of doing this, you will" +
+                " have a tree full of memories, and you can have fun remembering " +
+                "them all as you pull them out each year.\n" +
+                "\t\n" +
+                "    Letters: Rather than ornaments, enjoy a festive treat just for " +
+                "the two of you (hot cocoa, sparkling cider, eggnog, etc.) and exchange " +
+                "letters expressing your favorite memory from that year. If you want to " +
+                "get creative, you can give each year a theme (funniest memory, most" +
+                " meaningful, funniest parenting moment, most admirable moment, etc.).\n" +
+                "\t\n" +
+                "    Movie in Bed. This is the last official night for Christmas " +
+                "movies, so enjoy one together just the two of you before you drift " +
+                "off. Don’t have a TV in your room? Snuggle together with your phone," +
+                " iPad or laptop. Have a special toast for the new year.\n" +
+                "\t\n" +
+                "    Kitchen Festivities. Some couples really enjoy baking together " +
+                "(not me). A night of memories involves NOT being in the kitchen; " +
+                "however, I know of several friends that have special traditions " +
+                "that involve making some type of food the night before Christmas. " +
+                "One of my friends makes doughnuts with her husband every Christmas" +
+                " Eve, and another couple I know gets in their Christmas pajamas and" +
+                " eats Chinese food together. Find a food that would be fitting for the " +
+                "two of you, and enjoy it (or make it) together every year as a tradition.\n" +
+                "\t\n" +
+                "    Predictions. This is similar to the first two traditions, " +
+                "but rather than reflecting on the previous year, you make predictions" +
+                " for the year to come. Write down where you see yourselves exactly one " +
+                "year from now, and tuck the predictions away until the next year. This " +
+                "could be funny situations you see your partner getting him or herself" +
+                " into, promotions you can see happening, silly things you can imagine " +
+                "you will fight about, or funny things your children will most likely do. " +
+                "This is a fun one to do with some sparkling cider!\n" +
+                "\n" +
+                "That’s all I’ve got- do you have any more we can add to the list? " +
+                "Leave us a comment below!\n" +
+                "\n" +
+                "I wish you all the happiest of holidays!\n" +
+                "\n";
+        cheer_information_8.color = Color.rgb(122, 225, 222);
+        cheer_information_8.image = "";
+        cheer_information_8.URL = "http://www.somewhatsimple.com/christmas-eve-traditions-couples/";
+        items.add(cheer_information_8);
+
+
+
+        return items;
+    }
+
+}
